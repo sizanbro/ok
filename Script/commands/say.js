56b9d2301c6721ace81b6b491aa3f14576b3aa06
Mahim Islam <mahimcraft@vp.pl>
2025-09-14 19:38:08 +0600

    Update say.js

--- Script/commands/say.js
+++ Script/commands/say.js
@@ -21,7 +21,7 @@ module.exports.run = async function({ api, event, args }) {
 		var languageToSay = (["ru","en","pr","ja", "tl", "bn"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
 		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
 		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=bn&client=tw-ob`, path);
 		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
 	} catch (e) { return console.log(e) };
 }
